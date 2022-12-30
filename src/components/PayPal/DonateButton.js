87e96464a194758bbaad30e4b4f73bc0cb264dde
Alison Lee <alison2686@gmail.com>
2022-12-29 18:29:32 -0800

    enable strict mode

--- src/components/PayPal/DonateButton.js
+++ src/components/PayPal/DonateButton.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 // import { Helmet } from 'react-helmet'
 import {
     DonateFormContainer
@@ -15,34 +15,34 @@ import {
 //     })
 
 const DonateButton = () => {
-    // const buttonId = 'donate-button'
-    // useEffect(() => {
-    //     const button = window.PayPal.Donation.Button({
-    //         env:'production',
-    //         hosted_button_id:'JPB9347CK6TZE',
-    //         image: {
-    //         src:'https://pics.paypal.com/00/s/NDQ1NTRjMDktMGFmNC00NmFjLTg3NzMtOGI2YTBkM2QzMWY1/file.PNG',
-    //         alt:'Donate with PayPal button',
-    //         title:'PayPal - The safer, easier way to pay online!',
-    //         }
-    //     });
-    //     button.render(`#${buttonId}`); // you can change the code and run it when DOM is ready
-    // }, [buttonId]);
+    const buttonId = 'donate-button'
+    useEffect(() => {
+        const button = window.PayPal.Donation.Button({
+            env:'production',
+            hosted_button_id:'JPB9347CK6TZE',
+            image: {
+            src:'https://pics.paypal.com/00/s/NDQ1NTRjMDktMGFmNC00NmFjLTg3NzMtOGI2YTBkM2QzMWY1/file.PNG',
+            alt:'Donate with PayPal button',
+            title:'PayPal - The safer, easier way to pay online!',
+            }
+        });
+        button.render(`#${buttonId}`); // you can change the code and run it when DOM is ready
+    }, [buttonId]);
     return (
         
         <>
         <DonateFormContainer>
         {/* <div id="paypal-donate-button-container"></div> */}
-        {/* <div id={buttonId} /> */}
+        <div id={buttonId} />
         {/* <div id={containerId}>
         <div id={buttonId}></div>
         {paypal.render(`#${buttonId}`)} */}
         
-            <form action="https://www.paypal.com/donate" method="post" target="_top">
+            {/* <form action="https://www.paypal.com/donate" method="post" target="_top">
             <input type="hidden" name="hosted_button_id" value="ZJNQ8XKDM8H86" />
             <input type="image" src="https://pics.paypal.com/00/s/NDQ1NTRjMDktMGFmNC00NmFjLTg3NzMtOGI2YTBkM2QzMWY1/file.PNG" border="0" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button" />
             <img alt="" border="0" src="https://www.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1" />
-            </form>
+            </form> */}
         </DonateFormContainer>
         </>
     );
