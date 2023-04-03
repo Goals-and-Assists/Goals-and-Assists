ff6fed725a1249cff584421bfca88a9e65a70948
Alison Lee <alison2686@gmail.com>
2023-04-02 19:11:28 -0700

    remove width and max-width from InfoElements

--- src/components/InfoSection/InfoElements.js
+++ src/components/InfoSection/InfoElements.js
@@ -13,8 +13,6 @@ export const InfoWrapper = styled.div`
   display: grid;
   z-index: 1;
   height: 860px;
-  width: 100%;
-  /* max-width: 1100px; */
   margin-right: auto;
   margin-left: auto;
   padding: 0 24px;
